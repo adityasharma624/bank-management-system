3b6adca446e4a08d6fde5c21aaaac6380eaae453
Aditya <adityas.cse@gmail.com>
2026-03-30 20:00:37 +0530

    added savings and current account. but something feels off, i should be able to access savings and current from the menu after opening, i shouldn't have to create two seperate objects for it. will fix this.

---
+++ src/Accounts.java
@@ -0,0 +1,66 @@
+public abstract class Accounts {
+    int id;
+    String accountHolderName;
+//    int pin;
+    Accounts(int id, String accountHolderName) {
+        this.id = id;
+        this.accountHolderName = accountHolderName;
+    }
+
+    abstract void deposit(float amount);
+    abstract void withdraw(float amount);
+}
+
+class Savings extends Accounts{
+    float minimumBalance;
+    float balance;
+
+    Savings(int id, String accountHolderName, int minimumBalance, int balance) {
+        super(id, accountHolderName);
+        this.minimumBalance = minimumBalance;
+        this.balance = balance;
+    }
+
+    void deposit(float amount) {
+        this.balance += amount;
+        System.out.printf("Transaction successful. Amount deposited: %.2f. Updated Savings Account balance: %.2f.%n", amount, this.balance);
+    }
+
+    void withdraw(float amount) {
+        float remaining = this.balance - amount;
+        if (remaining < minimumBalance) {
+            System.out.printf(
+                    "Transaction declined: the requested withdrawal would result in the account falling below the required minimum balance of %.2f.%n",
+                    this.minimumBalance
+            );
+        }
+        else {
+            this.balance = remaining;
+            System.out.printf("Transaction successful. Amount withdraw: %.2f. Updated Savings Account balance: %.2f.%n", amount, this.balance);
+        }
+    }
+}
+
+class Current extends Accounts {
+    float balance;
+
+    Current(int id, String accountHolderName, int balance) {
+        super(id, accountHolderName);
+        this.balance = balance;
+    }
+
+    void deposit(float amount) {
+        this.balance += amount;
+        System.out.printf("Transaction successful. Amount deposited: %.2f. Updated Current Account balance: %.2f.%n", amount, this.balance);
+    }
+
+    void withdraw(float amount) {
+        float remaining = this.balance - amount;
+        if (remaining < 0) {
+            System.out.print("Transaction declined: insufficient available balance to process the requested withdrawal.");
+        }
+        else {
+            System.out.printf("Transaction successful. Amount withdraw: %.2f. Updated Current Account balance: %.2f.%n", amount, this.balance);
+        }
+    }
+}
