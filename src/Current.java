1271554e0fbc9384ca65ade532d151c8d389aa95
Aditya <adityas.cse@gmail.com>
2026-03-30 20:11:46 +0530

    seperated the classes

---
+++ src/Current.java
@@ -0,0 +1,23 @@
+public class Current extends Accounts {
+    float balance;
+
+    Current(int id, String accountHolderName, float balance) {
+        super(id, accountHolderName);
+        this.balance = balance;
+    }
+
+    void deposit(float amount) {
+        this.balance += amount;
+        System.out.printf("Transaction successful. Amount deposited: %.2f. Updated Current Account balance: %.2f.%n", amount, this.balance);
+    }
+
+    void withdraw(float amount) {
+        float remaining = this.balance - amount;
+        if (remaining < 0) {
+            System.out.print("Transaction declined: insufficient available balance to process the requested withdrawal.");
+        }
+        else {
+            System.out.printf("Transaction successful. Amount withdraw: %.2f. Updated Current Account balance: %.2f.%n", amount, this.balance);
+        }
+    }
+}
---
+++ src/Savings.java
@@ -0,0 +1,34 @@
+public class Savings extends Accounts{
+    float minimumBalance = 10000f;
+    float balance;
+
+    Savings(int id, String accountHolderName, float balance) {
+        super(id, accountHolderName);
+        this.balance = balance;
+    }
+
+    Savings(int id, String accountHolderName, float minimumBalance, float balance) {
+        super(id, accountHolderName);
+        this.minimumBalance = minimumBalance;
+        this.balance = balance;
+    }
+
+    void deposit(float amount) {
+        this.balance += amount;
+        System.out.printf("Transaction successful. Amount deposited: %.2f. Updated Savings Account balance: %.2f.%n", amount, this.balance);
+    }
+
+    void withdraw(float amount) {
+        float remaining = this.balance - amount;
+        if (remaining < minimumBalance) {
+            System.out.printf(
+                    "Transaction declined: the requested withdrawal would result in the account falling below the required minimum balance of %.2f.%n",
+                    this.minimumBalance
+            );
+        }
+        else {
+            this.balance = remaining;
+            System.out.printf("Transaction successful. Amount withdraw: %.2f. Updated Savings Account balance: %.2f.%n", amount, this.balance);
+        }
+    }
+}
